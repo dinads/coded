056b4accb87c79512b93b8134b391a74397605f3
dinads <90200168+dinads@users.noreply.github.com>
2021-09-15 15:26:12 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.py
@@ -0,0 +1,12 @@
+def on_forever():
+    pass
+    
+    basic.show_arrow(ArrowNames.NORTH)
+    basic.clear_screen()
+    basic.show_arrow(ArrowNames.EAST)
+    basic.clear_screen()
+    basic.show_arrow(ArrowNames.SOUTH)
+    basic.clear_screen()
+    basic.show_arrow(ArrowNames.WEST)
+basic.forever(on_forever)
+    
---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function on_forever() {
+    
+    basic.showArrow(ArrowNames.North)
+    basic.clearScreen()
+    basic.showArrow(ArrowNames.East)
+    basic.clearScreen()
+    basic.showArrow(ArrowNames.South)
+    basic.clearScreen()
+    basic.showArrow(ArrowNames.West)
+})
